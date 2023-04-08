62d4e92c556046d7e720484bc125ed28af5d4553
Shakeb Siddiqui <sms8508@psu.edu>
2023-04-08 18:39:52 -0400

    other page questions

--- app/Operational/page.tsx
+++ app/Operational/page.tsx
@@ -1,17 +1,32 @@
 import React from 'react';
 import Link from 'next/link';
+import { useState } from "react";
+import Checkbox from '../Checkbox';
+
 
 function Operational() {
-  return (
-    <>
-
-      <title>Operational</title>
-
-        <div>
-          <h1>Operational Risks</h1>
-        </div>
-    </>
-  );
-}
+    const checkboxQuestions = [
+      { label: "Do you like cats?", value: "cats" }, //Risk questions
+      { label: "Do you like dogs?", value: "dogs" },
+      { label: "Do you like fish?", value: "fish" },
+      { label: "Do you like birds?", value: "bird" },
+  
+    ];
+    return (
+      <>
+  
+        <title>Operational</title>
+  
+          <div>
+            <h1>Operational</h1>
+          </div>
+          <div>
+          <Checkbox questions={checkboxQuestions} />
+          </div>
+  
+  
+      </>
+    );
+    }
 
 export default Operational;
--- app/Technical/page.tsx
+++ app/Technical/page.tsx
@@ -1,17 +1,31 @@
 import React from 'react';
 import Link from 'next/link';
+import { useState } from "react";
+import Checkbox from '../Checkbox';
 
-function Financial() {
-  return (
-    <>
 
-      <title>Technical</title>
-
-        <div>
-          <h1>Technical Risks</h1>
-        </div>
-    </>
-  );
-}
-
-export default Financial;
+function Technical() {
+    const checkboxQuestions = [
+      { label: "Do you like cats?", value: "cats" }, //Risk questions
+      { label: "Do you like dogs?", value: "dogs" },
+      { label: "Do you like fish?", value: "fish" },
+      { label: "Do you like birds?", value: "bird" },
+  
+    ];
+    return (
+      <>
+  
+        <title>Technical</title>
+  
+          <div>
+            <h1>Technical Risks</h1>
+          </div>
+          <div>
+          <Checkbox questions={checkboxQuestions} />
+          </div>
+  
+  
+      </>
+    );
+    }
+export default Technical;
